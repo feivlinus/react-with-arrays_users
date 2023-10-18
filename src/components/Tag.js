ef2cdd259b72d9eaed15e7deb02f5fb8547e87f8
Felix Schmidt <para.feivl.dox@gmail.com>
2023-10-18 13:11:32 +0200

    Challenge completed

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,5 +1,6 @@
 import "./Tag.css";
 
 export default function Tag({ tag }) {
-  return <li className="tag">{tag}</li>;
+  const className = tag === "admin" ? "tag tag--highlight" : "tag";
+  return <li className={className}>{tag}</li>;
 }
